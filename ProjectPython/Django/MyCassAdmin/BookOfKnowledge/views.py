609a132b007d3d83085898cab3df1a0a5a48123d
AntonFender <antonfender@mail.ru>
2019-08-05 00:34:20 +0700

    Django

--- ProjectPython/Django/MyCassAdmin/BookOfKnowledge/views.py
+++ ProjectPython/Django/MyCassAdmin/BookOfKnowledge/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-	return HttpResponse("<a href=http://127.0.0.1:8000/Command><p align=center><font size=20>НАЖМИ НА МЕНЯ!!!</font></p></a>")
+	return render(request, 'GlavSTR/GlavPage.html')
--- ProjectPython/Django/MyCassAdmin/prog/views.py
+++ ProjectPython/Django/MyCassAdmin/prog/views.py
@@ -3,4 +3,6 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-	return HttpResponse("<a href=http://127.0.0.1:8000/Command><p align=center><font size=20>НАЖМИ НА МЕНЯ!!!</font></p></a>")
+	#return HttpResponse("<a href=http://127.0.0.1:8000/Command><p align=left><font size=6>Тех.поддержка</font></p></a><br><a href=http://127.0.0.1:8000/Programma><p align=left><font size=6>Администрирование</font></p></a>")
+	return render(request, 'Shablonizatot/Administrirovanie.html')
+	
