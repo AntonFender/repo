eead6d0ade693c00ec3f391c0de2c21afbb85224
AntonFender <antonfender@mail.ru>
2019-10-12 01:06:29 +0700

    add

--- ProjectPython/Django/MyCassAdmin/BookOfKnowledge/static/test/js/test.js
+++ ProjectPython/Django/MyCassAdmin/BookOfKnowledge/static/test/js/test.js
@@ -1,10 +1,30 @@
 'use strict';
 
+var arr = [12, 19, 28, 13, 14, 345];
+var result = [];
 
-var num = 500;
-while (num > 10) {
-	num = num / 2;
-	document.write(num + '<br>')
+for (var i = 0; i < 10; i++) {
+	if (inRange(arr[i])) {
+		result.push(arr[i]);
+	}
 }
-alert(num); //после цикла получим результат
+console.log(result);
+
+function inRange(num) {
+	var sum = arraySum(getDigits(num));
+	return sum >= 1 && sum <= 9;
+}
+
+function getDigits(num) {
+	return String(num).split('');
+}
+
+function arraySum(arr) {
+	var sum = 0;
+	for (var i = 0; i < arr.length; i++) {
+		sum += Number(arr[i]);
+	}
+
+	return sum;
+}
 
--- ProjectPython/Django/MyCassAdmin/RunServer.bat
+++ ProjectPython/Django/MyCassAdmin/RunServer.bat
@@ -1,4 +1,4 @@
 chcp 1251>nul
-cd C:\Users\Мартынов\Desktop\MyProgramm\repo\ProjectPython\Django\MyCassAdmin
+cd C:\Users\Anton\Desktop\repo\ProjectPython\Django\MyCassAdmin
 python manage.py runserver 0.0.0.0:8000
 pause
